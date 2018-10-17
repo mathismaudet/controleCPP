5a0812ac72b03d978883014187c76ceb60c951bc
mathis maudet <mathis.maudet@epsi.fr>
2018-10-17 12:12:42 +0200

    exit et supprimer 1

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,19 @@ int main ()
 	switch (option)
 	{
 	case 1 :{
+		ofstream f_nombres;
+		f_nombres.open("liste.txt");
+
 		for (int i = 0; i < 1; i++) {
 		cout << "Entrez un réel" << endl;
 		cin >> number;
 
 		liste.push_back(nombre());
 		liste[i].nombres = number;
+
+		f_nombres << liste[i].nombres << " " << endl;
 		}
+		f_nombres.close();
 
 		cout << "Votre valeur a été ajoutée dans la liste" << endl;
 
@@ -45,7 +51,15 @@ int main ()
 	break;}
 	case 3 :{
 		cout << "Entrez le réel que vous voulez supprimer" << endl;
-		
+		cin >> number;
+
+		for (int i = 0; i < liste.size(); i++) {
+			if (number == liste[i].nombres) {
+				liste.erase (liste.begin());
+				cout << number << " a été supprimé de la liste" << endl;
+			}
+		}
+
 		goto start;
 	break;}
 	case 4 :{
@@ -54,7 +68,10 @@ int main ()
 		goto start;
 	break;}
 	case 5 :{
-		
+		goto end;
+		end:
+
+		return 0;
 	break;}
 	}
 
